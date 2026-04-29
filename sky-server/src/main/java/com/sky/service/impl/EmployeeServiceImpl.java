87c139b764d42f5ff98371ac9a3fff276aa3f695
Amadeus <2593251697@qq.com>
2026-04-29 16:14:11 +0800

    添加新增员工方法,使用ThreadLocal传递登录ID

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -54,5 +60,36 @@ public class EmployeeServiceImpl implements EmployeeService {
         //3、返回实体对象
         return employee;
     }
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
 
+        Employee employee = new Employee();
+
+        //拷贝对象属性
+        BeanUtils.copyProperties(employeeDTO,employee);
+
+        //设置账号状态,默认为启用
+        employee.setStatus(StatusConstant.ENABLE);
+
+        //设置密码,默认为123456
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        //设置创建时间与更新时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //设置创建者与更新者ID
+        //从ThreadLocal当中中获取当前登录用户的ID
+        long userId = BaseContext.getCurrentId();
+        employee.setCreateUser(userId);
+        employee.setUpdateUser(userId);
+
+        //保存数据
+        employeeMapper.insert(employee);
+    }
 }
