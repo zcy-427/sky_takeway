558db0939072bc5730317e10da48e4509cfe8652
Amadeus <2593251697@qq.com>
2026-05-06 15:08:43 +0800

    feat: 新增阿里云OSS文件上传功能，并移除敏感配置追踪

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,50 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Slf4j
+@Api(tags = "公共通用接口")
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    @ApiOperation("文件上传接口")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传：{}", file);
+
+
+        try {
+            //获取文件原始名
+            String originalFilename = file.getOriginalFilename();
+            //获取文件后缀名
+            String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //生成新的文件名，防止文件名重复
+            String objectName = UUID.randomUUID().toString() + suffix;
+            //文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e.getMessage());
+        }
+
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
