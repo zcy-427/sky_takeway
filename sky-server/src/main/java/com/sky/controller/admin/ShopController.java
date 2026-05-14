f1f746b486c505e3f3f41a6396e78357907b1191
Amadeus <2593251697@qq.com>
2026-05-14 14:42:45 +0800

    新增店铺管理接口,查询修改店铺状态 修改接口文档分用户端与管理端

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,49 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Slf4j
+@Api(tags = "店铺管理相关接口")
+public class ShopController {
+
+    public static final String KEY="SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 修改店铺状态接口
+     *
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("修改店铺状态接口")
+    public Result SetStatus(@PathVariable Integer status) {
+        log.info("修改店铺状态：{}", status);
+        redisTemplate.opsForValue().set(KEY, status);
+        return Result.success();
+    }
+
+    /**
+     * 查询店铺状态接口
+     *
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("查询店铺状态接口")
+    public Result<Integer> getStatus() {
+        log.info("查询店铺状态");
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("店铺状态：{}", status==1?"营业中":"打烊中");
+        return Result.success(status);
+    }
+}
