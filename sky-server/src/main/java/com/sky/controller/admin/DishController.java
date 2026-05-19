04fbb16fb248d736a91346301d7c793869c27624
Amadeus <2593251697@qq.com>
2026-05-19 16:55:23 +0800

    新增菜品缓存功能,将菜品数据缓存到redis

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,9 +12,11 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.Put;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * 菜品管理
@@ -26,6 +28,8 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 新增菜品接口
@@ -37,6 +41,9 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
+        //新增菜品后需要清除redis中菜品数据，以便下次查询时能够获取到最新数据
+        String key="dish_"+dishDTO.getCategoryId();
+        clearCache(key);
         return Result.success();
     }
 
@@ -63,6 +70,11 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids) {
         log.info("删除菜品：ids={}", ids);
         dishService.deleteBatch(ids);
+
+        //todo:感觉这里没有必要,因为只能够删除起售中的菜品,所以可以放在起售停售接口中,当起售停售时直接清除redis中菜品数据,以便下次查询时能够获取到最新数据
+        //删除菜品后直接清除redis中所有菜品数据，以便下次查询时能够获取到最新数据
+        clearCache("dish_*");
+
         return Result.success();
     }
 
@@ -89,6 +101,9 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品信息：{}", dishDTO);
         dishService.updateWithFlavor(dishDTO);
+        //修改菜品后需要清除redis中菜品数据，以便下次查询时能够获取到最新数据
+        clearCache("dish_*");
+
         return Result.success();
     }
     /**
@@ -109,6 +124,19 @@ public class DishController {
     public Result<String> startOrStop(@PathVariable Integer status, Long id) {
         log.info("启用或禁用菜品：{},{}", status, id);
         dishService.startOrStop(status, id);
+        //启用或禁用菜品后需要清除redis中菜品数据，以便下次查询时能够获取到最新数据
+        clearCache("dish_*");
+
         return Result.success();
+    }
+
+    /**
+     * 清理缓存数据
+     * @param pattern
+     */
+    private void clearCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+
     }
 }
