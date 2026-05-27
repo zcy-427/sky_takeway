e4d6f75627626aee62d4ffaf8251321ca9ac0c67
Amadeus <2593251697@qq.com>
2026-05-27 15:35:42 +0800

    新增使用spring cache注解实现套餐缓存功能，存入redis

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,6 +32,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("新增套餐接口")
+    @CacheEvict(value = "setmealCache",key = "#setmealDTO.categoryId")//新增套餐后需要清除redis中套餐数据，以便下次查询时能够获取到最新数据
     public Result save(@RequestBody SetmealDTO setmealDTO) {
         log.info("新增套餐");
         setmealService.saveWithDish(setmealDTO);
@@ -57,6 +59,7 @@ public class SetmealController {
      */
     @DeleteMapping
     @ApiOperation("删除套餐接口")
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids) {
         log.info("删除套餐：ids={}", ids);
         setmealService.deleteBatch(ids);
@@ -83,6 +86,7 @@ public class SetmealController {
      */
     @PutMapping
     @ApiOperation("修改套餐接口")
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO) {
         log.info("修改套餐：{}", setmealDTO);
         setmealService.updateWithDish(setmealDTO);
@@ -91,6 +95,7 @@ public class SetmealController {
 
     @PostMapping("/status/{status}")
     @ApiOperation("启用或禁用套餐接口")
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public Result startOrStop(@PathVariable Integer status, long id) {
         log.info("启用或禁用套餐：{},{}", status, id);
         setmealService.startOrStop(status, id);
